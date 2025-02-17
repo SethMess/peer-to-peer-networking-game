76fae433f4f44f4aac96575bc792112df3ef3915
mediocreomen <73318172+mediocreomen@users.noreply.github.com>
2025-02-16 19:05:28 -0800

    Made basic server that can create lobbies and check for those wishing to join them using GET and POST API calls. Will include docs when I have the connection system finalized.

---
+++ server/siteserver.ts
@@ -0,0 +1,98 @@
+
+import { Sono } from 'https://deno.land/x/sono@v1.1/mod.ts';
+
+const sono = new Sono();
+const PORT = 3000
+
+class Lobby {
+  id = "";
+  name = "unamed_lobby";
+  max_players = 4;
+  players = Array<string>;
+};
+
+let lobby_list : Array<Lobby> = []; // Holds all current lobbies
+let id_list : Array<string> = []; // Holds all current lobby IDs to assure none are reused
+
+function generateLobbyID(length : number): string {
+  // Generates a random hexidecimal code of length provided
+  // all letters are lowercase, numbers are 0-9
+
+  let new_id = "";
+
+  for (let i = 0; i < length; i++) {
+    new_id += Math.floor(Math.random() * 16).toString(16);
+  }
+
+  return new_id;
+}
+
+function generateUniqueLobbyID(length : number): string {
+  // Generates a lobby id of given length that is not in use
+
+  let new_id = "";
+
+  while (new_id === "" || id_list.includes(new_id)) {
+    new_id = generateLobbyID(length);
+  }
+
+  return new_id;
+}
+
+Deno.serve({ port: PORT, hostname: "127.0.0.1" }, async (req) => {
+
+  const url = new URL(req.url);
+  const split_path = url.pathname.split("/");
+
+  let body = "";
+  if (req.body) {
+    body = await req.text();
+  }
+
+  // Index Page
+  if (req.method === "GET" && url.pathname === "/") {
+    console.log("FETCH INDEX")
+    const file = await Deno.open("./index.html", { read: true });
+    return new Response(file.readable);
+  }
+
+  // Game login
+  else if (req.method === "GET" && url.pathname === "/ws") {
+    sono.connect(req, () => {
+      sono.emit('new client connected')
+    });
+  }
+
+  // Get server list
+  else if (req.method === "GET" && url.pathname === "/lobbies") {
+    return new Response(JSON.stringify(lobby_list), { status: 200 });
+  }
+
+  // Connect to server
+  else if (req.method === "GET" && split_path.length == 3 && split_path[1] === "lobbies") {
+    if (id_list.includes(split_path[2])) {
+      return new Response("Yeah that lobby exists!", { status: 200 });
+    }
+    else {
+      return new Response("Lobby not found!", { status: 404 });
+    }
+  }
+
+  // Add a new server to the server list with the desired name, respond with the lobby object created
+  else if (req.method === "POST" && url.pathname === "/lobbies/new") {
+    const  new_lobby = new Lobby();
+    new_lobby.name == body.substring(0, 32); // Cap srver names at 32 characters
+
+    new_lobby.id = generateUniqueLobbyID(6); // Generate and log lobby id, create channel of that name
+    id_list.push(new_lobby.id);
+    sono.channel('new_lobby.id', () => {
+      console.log('new lobby channel created: ' + new_lobby.id);
+    })
+
+    lobby_list.push(new_lobby);
+
+    return new Response(JSON.stringify(new_lobby), { status: 200 });
+  }
+
+  return new Response("404 Not Found", { status: 404 });
+});
