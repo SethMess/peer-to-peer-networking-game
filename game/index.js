68e0f89799c489f9686a811f7429fd26581330f3
SethMess <sethwmessier@gmail.com>
2025-03-06 14:19:48 -0800

    added enemies and proper projectiles

--- game/index.js
+++ game/index.js
@@ -37,7 +37,7 @@ class Projectile {
     draw() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        // c.strokeStyle = this.color;
+        c.strokeStyle = this.color;
         c.fillStyle = this.color;
         c.stroke();
         c.fill();
@@ -47,33 +47,112 @@ class Projectile {
     update() {
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
+        this.draw();
     }
 }
 
 
+class Enemy {
+    constructor(x, y, radius, color, velocity){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        c.strokeStyle = this.color;
+        c.fillStyle = this.color;
+        c.stroke();
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+        this.draw();
+    }
+}
+
+
+
 function animate(){
     requestAnimationFrame(animate);
+
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    player.draw();// console.log(projectiles);
+    
+
+    projectiles.forEach((proj) => {
+        proj.update();
+    })
+
+    enemies.forEach((enemy) => {
+        enemy.update();
+    })
+
+    
+    
 }
+
+
+// function collisionDetection
 
 //Event listeners
 addEventListener('click', (event) => {
     console.log("spawn");
-    let angle = Math.atan2(event.clientY, event.clientX);
-    let velocity = {x: Math.cos(angle), y: Math.sin(angle)};
-    const projectile = new Projectile(event.clientX, event.clientY, 10, 'black', velocity);
-    projectile.draw();
+    let angle = Math.atan2(event.clientY - player.y, event.clientX - player.x);
+    let velocity = {x: Math.cos(angle) * 2, y: Math.sin(angle) * 2};
+    // velocity = velocity * 2;
+    const projectile = new Projectile(player.x, player.y, 5, 'green', velocity);
+    projectiles.push(projectile);
+
 });
+
+
+function spawnEnemies(){
+    setInterval(() => {
+        const radius = Math.random() * (30 - 10) + 10;
+        let x;
+        let y;
+        if(Math.random() < 0.5){
+            x = Math.random() < 0.5 ? 0 - radius: canvas.width + radius;
+            y = Math.random() * canvas.height;
+        }else{
+            x = Math.random() * canvas.width;            
+            y = Math.random() < 0.5 ? 0 - radius: canvas.height + radius;
+        }
+        
+        let angle = Math.atan2(player.y - y, player.x - x);
+        let velocity = {x: Math.cos(angle) * 2, y: Math.sin(angle) * 2};
+        enemies.push(new Enemy(x, y, radius, "purple", velocity));
+    }, 1000)
+}
+
 
 //MAIN area
 
 let x = canvas.width / 2;
 let y = canvas.height / 2;
 
+
+const projectiles = [];
+const enemies = [];
+
 const player = new Player(x, y, 30, 'blue');
 player.draw();
 
-let projectile = new Projectile(200, 200, 10, 'red', null);
-projectile.draw();
+spawnEnemies();
+// let projectile = new Projectile(player.x, player.y, 5, 'red', {x: 1, y: 1});
+// projectile.draw();
+// projectile.update();
+// projectiles.push(projectile);
 
 
 animate();
