1fbeeddfb12903c122960346d364cf695fec0184
SethMess <sethwmessier@gmail.com>
2025-03-09 23:06:39 -0700

    worked on the game, and added collision detection and keyboard input, I also updated the website and readme to include our website link.

--- game/index.js
+++ game/index.js
@@ -3,6 +3,8 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 const c = canvas.getContext('2d');
 
+const scoreEl = document.querySelector('#scoreEl')
+
 
 console.log("YIPEEE", canvas);
 
@@ -80,12 +82,14 @@ class Enemy {
 
 
 
+let animationId
+let score = 0
 function animate() {
-    requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate);
 
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-
+    handleMovement();
     player.draw();// console.log(projectiles);
 
 
@@ -97,7 +101,7 @@ function animate() {
         enemy.update();
     })
 
-
+    collisionDetection();
 
 }
 
@@ -135,6 +139,84 @@ function spawnEnemies() {
     }, 1000)
 }
 
+function collisionDetection() {
+    for (let index = enemies.length - 1; index >= 0; index--) {
+        const enemy = enemies[index]
+
+        // enemy.update()
+
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        //end game
+        if (dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId)
+        }
+
+        for (
+            let projectilesIndex = projectiles.length - 1;
+            projectilesIndex >= 0;
+            projectilesIndex--
+        ) {
+            const projectile = projectiles[projectilesIndex]
+
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            // when projectiles touch enemy
+            if (dist - enemy.radius - projectile.radius < 1) {
+                // create explosions
+                // for (let i = 0; i < enemy.radius * 2; i++) {
+                //     particles.push(
+                //         new Particle(
+                //             projectile.x,
+                //             projectile.y,
+                //             Math.random() * 2,
+                //             enemy.color,
+                //             {
+                //                 x: (Math.random() - 0.5) * (Math.random() * 6),
+                //                 y: (Math.random() - 0.5) * (Math.random() * 6)
+                //             }
+                //         )
+                //     )
+                // }
+                // this is where we shrink our enemy
+                if (enemy.radius - 10 > 5) {
+                    score += 100;
+                    scoreEl.innerHTML = score;
+                    enemy.radius -= 10;
+
+                    projectiles.splice(projectilesIndex, 1)
+                } else {
+                    // remove enemy if they are too small
+                    score += 150;
+                    scoreEl.innerHTML = score;
+
+                    enemies.splice(index, 1);
+                    projectiles.splice(projectilesIndex, 1);
+                }
+            }
+        }
+    }
+}
+
+
+function handleMovement() {
+    if (keys.w.pressed) {
+        player.y -= 1;
+    }
+
+    if (keys.a.pressed) {
+        player.x -= 1;
+    }
+
+    if (keys.s.pressed) {
+        player.y += 1;
+    }
+
+    if (keys.d.pressed) {
+        player.x += 1;
+    }
+}
+
 
 //MAIN area
 
@@ -146,7 +228,23 @@ const projectiles = [];
 const enemies = [];
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+// player.draw();
 
 spawnEnemies();
 // let projectile = new Projectile(player.x, player.y, 5, 'red', {x: 1, y: 1});
@@ -156,3 +254,85 @@ spawnEnemies();
 
 
 animate();
+
+
+// Keydown event listener
+
+
+
+// Example of how we might send key inputs to other players, but in a peer to peer game we would just send to the peers instead
+/*
+setInterval(() => {
+    if (keys.w.pressed) {
+      sequenceNumber++
+      playerInputs.push({ sequenceNumber, dx: 0, dy: -SPEED })
+      // frontEndPlayers[socket.id].y -= SPEED
+      socket.emit('keydown', { keycode: 'KeyW', sequenceNumber })
+    }
+
+    if (keys.a.pressed) {
+      sequenceNumber++
+      playerInputs.push({ sequenceNumber, dx: -SPEED, dy: 0 })
+      // frontEndPlayers[socket.id].x -= SPEED
+      socket.emit('keydown', { keycode: 'KeyA', sequenceNumber })
+    }
+
+    if (keys.s.pressed) {
+      sequenceNumber++
+      playerInputs.push({ sequenceNumber, dx: 0, dy: SPEED })
+      // frontEndPlayers[socket.id].y += SPEED
+      socket.emit('keydown', { keycode: 'KeyS', sequenceNumber })
+    }
+
+    if (keys.d.pressed) {
+      sequenceNumber++
+      playerInputs.push({ sequenceNumber, dx: SPEED, dy: 0 })
+      // frontEndPlayers[socket.id].x += SPEED
+      socket.emit('keydown', { keycode: 'KeyD', sequenceNumber })
+    }
+  }, 15)
+  */
+
+window.addEventListener('keydown', (event) => {
+    //check if its a front end player and return if not
+
+    switch (event.code) {
+        case 'KeyW':
+            keys.w.pressed = true
+            break
+
+        case 'KeyA':
+            keys.a.pressed = true
+            break
+
+        case 'KeyS':
+            keys.s.pressed = true
+            break
+
+        case 'KeyD':
+            keys.d.pressed = true
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    //check if its a front end player and return if not
+
+    switch (event.code) {
+        case 'KeyW':
+            keys.w.pressed = false
+            break
+
+        case 'KeyA':
+            keys.a.pressed = false
+            break
+
+        case 'KeyS':
+            keys.s.pressed = false
+            break
+
+        case 'KeyD':
+            keys.d.pressed = false
+            break
+    }
+})
