7d3a4767ecb9b5404ba043b03f247bbb143f0c43
mediocreomen <73318172+mediocreomen@users.noreply.github.com>
2025-03-10 23:26:52 -0700

    Added RTC implementation that works with sono. Needs to not be on localhost to work though. The placeholder game.html/js pages are an example.

---
+++ server/static/RTC.js
@@ -0,0 +1,196 @@
+export class SonoRTC {
+  constructor(serverConfig, signalingServer){
+    this.configuration = serverConfig;
+    this.peerconnection = {}; // "local" sending connections
+    this.remoteconnections = {}; // "remote" recieveing connections
+    this.server = signalingServer;
+    this.callback = null;
+    this.dataStreams = {};
+    this.receiveChannels = {};
+    // this.eventListeners();
+    // this.localtracks = [];
+    // this.createOffer = this.createOffer.bind(this)
+    // this.createdOffer = null;
+  }
+  eventListeners(){
+    this.server.on('grab', (payload) => {
+      if(payload.type === 'clients'){
+        this.clients = payload.message;
+        if(this.mychannelclients && this.myid && this.mychannel){
+          this.server.trigger('createRTCs')
+        }
+      }
+      else if(payload.type === 'myid'){
+        this.myid = payload.message[0];
+        if(this.mychannelclients && this.clients && this.mychannel){
+          this.server.trigger('createRTCs')
+        }
+      }
+      else if(payload.type === 'mychannelclients'){
+        this.mychannelclients = payload.message;
+        if(this.clients && this.myid && this.mychannel){
+          this.server.trigger('createRTCs')
+        }
+      }
+      else if (payload.type === 'mychannel'){
+        this.mychannel = payload.message;
+        if(this.clients && this.myid && this.mychannelclients){
+          this.server.trigger('createRTCs')
+        }
+      }
+    })
+    this.server.on('sendingOffer', (payload)=> {
+      const from = payload.from;
+      const message = payload.message
+      if(message.type === 'offer'){
+        if(this.inRoom === true) this.startConnection();
+        else return;
+
+        this.remoteconnections[from].setRemoteDescription(new RTCSessionDescription(message))
+
+        this.remoteconnections[from].createAnswer()
+          .then(answer => {
+            this.remoteconnections[from].setLocalDescription(answer);
+            this.server.directmessage(answer, from, 'sendingAnswer')
+          })
+          .catch(err => console.log('error: unable to create answer', err))
+      }
+    })
+    this.server.on('sendingAnswer', (payload) => {
+      const from = payload.from;
+      const message = payload.message;
+      if(message.type === 'answer'){
+
+        this.peerconnection[from].setRemoteDescription(new RTCSessionDescription(message));
+      }
+    })
+    this.server.on('createRTCs', ()=> {
+
+      this.createRTCs();
+    })
+    this.server.on('icecandidate_peer', (payload) => {
+      const from = payload.from;
+      const message = payload.message;
+      this.peerconnection[from].addIceCandidate(message['new-ice-candidate'])
+        .catch(err => console.log('err', err))
+    })
+    this.server.on('icecandidate_remote', (payload) => {
+      const from = payload.from;
+      const message = payload.message;
+      this.remoteconnections[from].addIceCandidate(message['new-ice-candidate'])
+        .catch(err => console.log('err', err))
+    })
+
+    this.server.on('clientleaving', (payload) => {
+      console.log("LEAVING")
+      const from = payload.from;
+
+      delete this.peerconnection[from];
+
+      this.startConnection();
+    })
+    this.server.on('clientjoining', (payload) => {
+      console.log("JOINING")
+      this.startConnection();
+    })
+  }
+  async startConnection(){
+    if(!this.inRoom) await this.eventListeners();
+    this.inRoom = true;
+    this.server.grab('clients');
+    this.server.grab('myid');
+    this.server.grab('mychannelclients');
+    this.server.grab('mychannel');
+  }
+  createRTCs(){
+
+    this.mychannelclients.forEach(client => {
+
+      if(client === this.myid || this.peerconnection[client]){
+        return; // Already have one or dont need one
+      }
+
+      // Create new local peer and remote peer
+      this.peerconnection[client] = new RTCPeerConnection(this.configuration);
+      this.remoteconnections[client] = new RTCPeerConnection(this.configuration);
+
+      // Create new data stream
+      this.dataStreams[client] = this.peerconnection[client].createDataChannel(client);
+      
+      this.peerconnection[client].onnegotiationneeded = () => {
+        this.peerconnection[client].createOffer()
+          .then(createdOffer => {
+
+            this.peerconnection[client].setLocalDescription(createdOffer);
+            this.server.directmessage(createdOffer, client, 'sendingOffer');
+          })
+          .catch(err => console.log('err', err))
+      }
+      this.peerconnection[client].onicecandidate = (event) => {
+        if (event.candidate) {
+
+          const message = {'new-ice-candidate': event.candidate}
+          this.server.directmessage(message, client, 'icecandidate_remote');
+        }
+      }
+      this.remoteconnections[client].onicecandidate = (event) => {
+        if (event.candidate) {
+
+          const message = {'new-ice-candidate': event.candidate}
+          this.server.directmessage(message, client, 'icecandidate_peer');
+        }
+      }
+      this.peerconnection[client].onconnectionstatechange = (event) => {
+        if (this.peerconnection[client].connectionState === 'connected') {
+
+          console.log('connected with client id:', client);
+        }
+      }
+      this.remoteconnections[client].ondatachannel = (event) => {
+
+        this.receiveChannels[client] = event.channel;
+        console.log(this.callback)
+        this.receiveChannels[client].onmessage = this.callback;
+        // this.dataStreams[client].onopen = callbacks can be added if needed
+        // this.dataStreams[client].onclose = callbacks can be added if needed
+
+      };
+    })
+  }
+  changeChannel(targetChannel){
+
+    if(!this.inRoom && targetChannel === 'home'){
+      return this.startConnection();
+    }
+
+    if(targetChannel == this.mychannel){
+
+      return;
+    }
+
+    this.server.broadcast('clientleaving', 'clientleaving');
+
+    this.server.changeChannel(targetChannel);
+
+    this.server.broadcast('clientjoining', 'clientjoining');
+
+    this.peerconnection = {};
+    this.dataStreams = {};
+    this.receiveChannels = {};
+
+    this.startConnection();
+  }
+
+  sendMessage(message) {
+    // Send message to all clients
+    console.log(this.mychannelclients);
+    this.mychannelclients.forEach(client => {
+      if(client === this.myid){
+        // NOTHING FOR YOURSEWL
+      } else {
+        this.dataStreams[client].send(message);
+        console.log("Sending message to " + this.dataStreams[client]);
+      }
+    })
+  }
+}
--- server/static/game.js
+++ server/static/game.js
@@ -1,6 +1,21 @@
 import { SonoClient } from 'https://deno.land/x/sono@v1.2/src/sonoClient.js';
+import { SonoRTC } from "./RTC.js"
 
 const WS_URL = "ws://localhost:3001" // <- UPDATE TO CORRECT URL!!!
+const serverConfig = {
+    iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun.l.google.com:5349" },
+        { urls: "stun:stun1.l.google.com:3478" },
+        { urls: "stun:stun1.l.google.com:5349" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:5349" }
+    ]
+}
+
+const pingbuttonelm = document.getElementsByClassName("pingbutton")[0]
+
+let rtc_connection = null
 
 window.onload = function () {
     main();
@@ -11,18 +26,41 @@ function main() {
     let lobbyid = spliturl[spliturl.length - 1];
     let lobbynameelm = document.getElementsByClassName("lobbyname")[0];
     lobbynameelm.innerHTML = "LOBBY ID: " + lobbyid;
+    pingbuttonelm.disabled = true;
 
     const sono = new SonoClient(WS_URL + '/join/' + lobbyid);
-    waitForConnection(sono)
+    waitForConnection(sono, lobbyid)
 }
 
-function waitForConnection(sono) {
+function messageRecieved(message) {
+    alert(message.data);
+}
+
+function waitForConnection(sono, lobbyid) {
     // COde that waits until sono is fully connected
-    if(sono.ws.readyState == 0) {globalThis.setTimeout(function() {waitForConnection(sono)}, 1000); /* this checks the connectionReady flag every 1000 milliseconds*/ }
-    else {gameCode(sono)}
+    if(sono.ws.readyState == 0) {globalThis.setTimeout(function() {waitForConnection(sono, lobbyid)}, 1000); /* this checks the connectionReady flag every 1000 milliseconds*/ }
+    else {gameCode(sono, lobbyid)}
 }
 
-function gameCode(sono) {
+function gameCode(sono, lobbyid) {
     // Code for the game goes here
     alert("CONNECTED!");
+
+    // Now we setup the webrtc connection
+    rtc_connection = new SonoRTC(serverConfig, sono, {}) // No constraints for now
+    sono.changeChannel(lobbyid);
+    rtc_connection.changeChannel(lobbyid);
+    rtc_connection.callback = (message) => messageRecieved(message);
+
+    // Once that's done, enable the ping!
+    sono.broadcast("", "newconnection", (message) => messageRecieved(message));
+
+    // If anyone else joins and sends an arrival message, print it to the console
+    sono.on('newconnection', (payload) => {
+        console.log(payload); // Will look like { message: "Hello!", from: <client_id> } (client id will probably be 100X)
+        });
+    
+    pingbuttonelm.disabled = false;
+    pingbuttonelm.addEventListener("click", function() {rtc_connection.sendMessage("PING!")});
+    
 }
